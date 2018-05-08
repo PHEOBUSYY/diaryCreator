802c8790347506bcfef260eda9210945d9fe0866
yanyi <yanyi609@126.com>
2018-05-08 16:38:51 +0800

    modify css

--- src/vue/pages/htmlTableParser.vue
+++ src/vue/pages/htmlTableParser.vue
@@ -1,38 +1,33 @@
 <template>
     <div class="container">
-        <h2>Markdown Table Parser</h2>
-        <h3>html样式转换成markdown的table格式工具</h3>
-        <div class="input">
-            <el-select v-model="template" placeholder="模板" class="template_input" @change="templateChange">
-                <el-option
-                        v-for="item in options"
-                        :key="item.value"
-                        :label="item.label"
-                        :value="item.value">
-                </el-option>
-            </el-select>
-            /
-           
-           行
-           
-           
-           <el-input v-model.number="row" class="row_input"></el-input>
+        <div class="title">
+            <h2>Markdown Table Parser</h2>
+            <span>-html样式转换成markdown的table格式工具</span>
+        </div>
+        
+        <el-select v-model="template" placeholder="请选择模板" class="template_input"
+                   @change="templateChange">
+            <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value">
+            </el-option>
+        </el-select>
+        <div class="input" v-show='template'>
+            行
+            
+            <el-input v-model.number="row" class="row_input"></el-input>
             列
             
-            
             <el-input v-model.number="column" class="column_input"></el-input>
-            
-           
-            
-            
-            
         </div>
-        <h4>请输入表格内容</h4>
-        <table>
+        <h4 v-show="template">编辑表格内容</h4>
+        <table v-show="template">
             <tr v-for="(item,index) in realArray" :key="index">
                 <th v-if="index === 0" v-for="(item2,index2) in realArray[index]" :key="index2">
                     <el-input v-model="realArray[index][index2]"
-                              placeholder="表头"></el-input>
+                              :placeholder="'标题'+(index2+1)"></el-input>
                 </th>
                 <td v-if="index !==0" v-for="(item2,index2) in realArray[index]" :key="index2">
                     <el-time-select v-if="index2 < 2 && template === 'time'"
@@ -43,16 +38,17 @@
                                     placeholder="">
                     </el-time-select>
                     <el-input v-else v-model="realArray[index][index2]"
-                              ></el-input>
+                    ></el-input>
                 </td>
             </tr>
         </table>
         <!--<label>预览</label>-->
         <div ref="preview">
         </div>
-        <el-button type="primary" @click="btnClick" >{{state === 0 ? '点击生成': '清空'}}</el-button>
-        <h4>输出</h4>
-        <textarea class="output" ref="output" title="output" v-model="output"></textarea>
+        <button v-show="template" @click="btnClick">{{parsed === 0 ? '点击生成' : '清空'}}</button>
+        <h4 v-show="output">输出</h4>
+        <textarea v-show="output" class="output" ref="output" title="output"
+                  v-model="output"></textarea>
     </div>
 </template>
 <script>
@@ -66,12 +62,13 @@
                 testTime: '',
                 realArray: null,
                 pickOption: {
-                    start: '05:00', step: '00:15', end: '23:30'
+                    start: '05:00', step: '00:10', end: '23:30'
                 },
                 options: [
                     {
                         value: 'time',
-                        label: '时间记录'
+                        label: '时间记录',
+                        title: ['开始时间', '结束时间', '事项', '备注']
                     },
                     {
                         value: 'thinking',
@@ -82,7 +79,7 @@
                     }
                 ],
                 template: '',
-                state: 0
+                parsed: 0,
             }
         },
         computed: {},
@@ -100,7 +97,9 @@
                 for (let i = 0; i < this.row; i++) {        //一维长度为3
                     a[i] = [];
                     for (let j = 0; j < this.column; j++) {    //二维长度为5
-                        if (this.copyData && this.copyData.length > 0 && this.copyData.length > i && this.copyData[i].length > j && this.copyData[i][j]) {
+                        if (i === 0 && this.template === 'time' && this.options[0] && this.options[0].title && j < this.options[0].title.length) {
+                            a[i][j] = this.options[0].title[j];
+                        } else if (this.copyData && this.copyData.length > 0 && this.copyData.length > i && this.copyData[i].length > j && this.copyData[i][j]) {
                             a[i][j] = this.copyData[i][j];
                         } else {
                             a[i][j] = '';
@@ -113,14 +112,14 @@
                 this.realArray = a;
             },
             btnClick: function () {
-                if(!this.row || !this.column){
+                if (!this.row || !this.column) {
                     this.$message({
                         message: '请选择模板或者具体的行列数',
                         type: 'warning'
                     });
-                    return ;
+                    return;
                 }
-                if(this.state === 0){
+                if (this.parsed === 0) {
                     //存储
                     localStorage.setItem('data', JSON.stringify(this.copyData));
                     localStorage.setItem('row', this.row);
@@ -132,30 +131,30 @@
                         this.$refs.output.select();
                         document.execCommand("Copy");
                         navigator.clipboard.writeText(this.output);
-                        this.state = 1;
+                        this.parsed = 1;
                     });
-                }else{
+                } else {
                     this.copyData = [];
                     this.initRealArray();
                     localStorage.setItem('data', "");
                     this.$refs.preview.innerHTML = '';
                     this.output = '';
-                    this.state = 0;
+                    this.parsed = 0;
                     localStorage.setItem('row', '');
                     localStorage.setItem('column', '');
                 }
-                
+
 
             },
             parser: function () {
                 //生成一个row * column的table
                 let result = '<table>\n';
                 for (let i = 0; i < this.realArray.length; i++) {
-                    if (i % 2 === 0) {
-                        result += '  <tr style="background: #f2f2f2">\n';
-                    } else {
-                        result += '  <tr style="background: #ffffff">\n';
-                    }
+//                    if (i % 2 === 0) {
+//                        result += '  <tr>\n';
+//                    } else {
+                    result += '  <tr>\n';
+//                    }
                     let line = '';
                     let isEmpty = true;
                     for (let j = 0; j < this.realArray[i].length; j++) {
@@ -207,36 +206,43 @@
                 return result;
             },
             onTimeFocus: function (row, column) {
-                if(column === 1){
+                if (column === 1 && !this.realArray[row][0]) {
+                    this.$message({
+                        message: '请选择开始时间',
+                        type: 'warning'
+                    });
+                    return;
+                }
+                if (column === 1 && row > 1 && this.realArray[row][0]) {
                     this.$set(this.pickOption, 'start', this.realArray[row][0]);
                 }
             },
             onTimeChange: function (row, column) {
-
                 if (column === 1 && row + 1 < this.realArray.length && this.realArray[row][column]) {
                     //设置完第二列直接把下面那一行的第一列赋值
                     //注意这里vue无法监听数组中内容的变化,需要通过$set来重新赋值
                     let temp = this.realArray[row + 1].slice(0);
                     temp[0] = this.realArray[row][column];
                     this.$set(this.realArray, row + 1, temp);
-//                    this.$set(this.pickOption, 'start', this.realArray[row][0]);
                 }
             },
             templateChange: function () {
-                if(this.template === 'time'){
+                if (this.template === 'time') {
                     this.row = '15';
                     this.column = '4';
-                }else if(this.template === 'thinking'){
+                } else if (this.template === 'thinking') {
                     this.row = '10';
                     this.column = '2';
-                }else{
+                } else {
                     this.row = '3';
                     this.column = '3';
                 }
+                this.copyData = [];
+                this.initRealArray();
             }
         },
         mounted: function () {
-            if(localStorage.getItem('data')){
+            if (localStorage.getItem('data')) {
                 this.copyData = JSON.parse(localStorage.getItem('data'));
             }
             this.row = localStorage.getItem('row');
@@ -246,10 +252,26 @@
     }
 </script>
 <style lang="scss">
+    body {
+        background: #f67280;
+    }
+    
+    .el-input__inner {
+        border: none;
+        background: #c06c84;
+        color: white;
+    }
+    
+    .el-input__inner::placeholder {
+        color: white;
+    }
+    .el-icon-time:before{
+        color: #f3f3f3
+    }
     table, td, th {
         /*border: 1px solid #ddd;*/
         padding: 8px;
-        /*border-collapse: collapse;*/
+        border-collapse: collapse;
         font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
     }
     
@@ -261,6 +283,7 @@
         width: 90%;
         margin: 10px;
         padding: 10px;
+        background: inherit;
     }
     
     td, th {
@@ -278,10 +301,9 @@
     }
     
     th {
+        border-bottom: 1px solid #c6c6c6;
         padding-top: 12px;
         padding-bottom: 12px;
-        /*background-color: #bec0bf;*/
-        /*color: white;*/
     }
     
     tr:nth-child(even) {
@@ -296,30 +318,58 @@
     .container {
         display: flex;
         flex-direction: column;
+        padding: 10px;
+        color: white;
     }
-    .input{
+    
+    .input {
         display: flex;
         flex-direction: row;
         align-items: center;
+        margin: 20px;
     }
+    
     .output {
         height: 400px;
+        background: #c06c84;
+        color: white;
     }
     
     .row_input, .column_input {
         width: 100px;
         margin: 20px;
     }
+    
     button {
         height: 45px;
         width: 60%;
         margin: 10px auto;
-        /*background: #2c7dc9;*/
+        background: #355c7d;
         color: white;
         border-radius: 9px;
     }
+    
     .template_input {
         width: 200px;
         margin: 20px;
+    }
+    
+    .title {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        & > span {
+            margin: 0 10px;
+        }
+    }
+    @media screen and (max-width: 500px) {
+        .title {
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start;
+            & > span {
+                margin: 0 10px;
+            }
+        }
     }
 </style>
